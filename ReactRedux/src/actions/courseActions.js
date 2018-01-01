21b37d02cd2af5186bd2f2b684822c52442ca660
Tomasz Jaskula <thomasv1000@hotmail.fr>
2018-01-01 13:15:51 +0100

    Create actions for create and update course

--- ReactRedux/src/actions/courseActions.js
+++ ReactRedux/src/actions/courseActions.js
@@ -5,10 +5,29 @@ export function loadCoursesSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses };
 }
 
+export function createCourseSuccess(courses) {
+  return { type: types.CREATE_COURSE_SUCCESS, courses };
+}
+
+export function updateCourseSuccess(courses) {
+  return { type: types.UPDATE_COURSE_SUCCESS, courses };
+}
+
 export function loadCourses() {
   return function (dispatch) {
     return CourseApi.getAllCourses().then(courses => {
       dispatch(loadCoursesSuccess(courses));
+    }).catch(error => {
+      throw(error);
+    });
+  };
+}
+
+export function saveCourse(course) {
+  return function (dispatch, getState) {
+    return CourseApi.saveCourse(course).then(saveCourse => {
+      course.id ? dispatch(updateCourseSuccess(saveCourse)) :
+        dispatch(createCourseSuccess(saveCourse));
     }).catch(error => {
       throw(error);
     });
