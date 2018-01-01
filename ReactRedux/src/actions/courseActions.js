5a16c2697092f18733c1a2263858e022a9936e2e
Tomasz Jaskula <thomasv1000@hotmail.fr>
2018-01-01 15:05:07 +0100

    Add componentWillReceiveProps and fixing some bugs in course actions

--- ReactRedux/src/actions/courseActions.js
+++ ReactRedux/src/actions/courseActions.js
@@ -5,12 +5,12 @@ export function loadCoursesSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses };
 }
 
-export function createCourseSuccess(courses) {
-  return { type: types.CREATE_COURSE_SUCCESS, courses };
+export function createCourseSuccess(course) {
+  return { type: types.CREATE_COURSE_SUCCESS, course };
 }
 
-export function updateCourseSuccess(courses) {
-  return { type: types.UPDATE_COURSE_SUCCESS, courses };
+export function updateCourseSuccess(course) {
+  return { type: types.UPDATE_COURSE_SUCCESS, course };
 }
 
 export function loadCourses() {
@@ -25,9 +25,9 @@ export function loadCourses() {
 
 export function saveCourse(course) {
   return function (dispatch, getState) {
-    return CourseApi.saveCourse(course).then(saveCourse => {
-      course.id ? dispatch(updateCourseSuccess(saveCourse)) :
-        dispatch(createCourseSuccess(saveCourse));
+    return CourseApi.saveCourse(course).then(savedCourse => {
+      course.id ? dispatch(updateCourseSuccess(savedCourse)) :
+        dispatch(createCourseSuccess(savedCourse));
     }).catch(error => {
       throw(error);
     });
