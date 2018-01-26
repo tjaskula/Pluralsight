c7df64ddb09bfec079dd589a01420d01b6aef8ea
tjaskula <thomasv1000@hotmail.fr>
2018-01-26 14:46:40 +0100

    Delete testDocs

--- testDoc/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Docs from './docs/Docs';
-import registerServiceWorker from './registerServiceWorker';
-import '../node_modules/highlight.js/styles/ocean.css'
-
-ReactDOM.render(<Docs />, document.getElementById('root'));
-registerServiceWorker();
